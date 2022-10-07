6c0d773397daa5b8cd7ab539fb16eaef4c2e8e1f
Dalma Fernandez <dalmagiufernandez@gmail.com>
2022-10-07 16:58:55 -0300

    Primera parte de CRUD pacientes

---
+++ js/Paciente.js
@@ -0,0 +1,14 @@
+export class Paciente {
+    constructor(nombre, apellido, dni, fn, sexo, os, telefono, domicilio, email, desc) {
+      this.nombre = nombre;
+      this.apellido = apellido;
+      this.dni= dni;
+      this.fn= fn;
+      this.sexo = sexo;
+      this.os= os;
+      this.telefono = telefono;
+      this.domicilio = domicilio;
+      this.email = email;
+      this.desc = desc;
+    }
+  }
---
+++ js/adminPaciente.js
@@ -0,0 +1,103 @@
+import { Paciente } from "./Paciente.js";
+import { validateEmail, validateName, validateNumber } from "./validators.js";
+
+//const paciente1 = new Paciente ("Dalma", "Fernandez", "5451564652","15/2/96", "F", "SS", "64982564","Buenos Aires 500", "dalma@gmail.com","nada");
+//console.log(paciente1);
+const formularioPaciente = document.getElementById("formPaciente");
+
+const campoNombre = document.getElementById("nombrePaciente");
+const campoApellido = document.getElementById("apellidoPaciente");
+const campoDNI = document.getElementById("dniPaciente");
+const campoFecha = document.getElementById("fechaNacimiento");
+const campoSexo = document.getElementById("selectSexo");
+const campoObraSocial = document.getElementById("selectObraSocial");
+const campoTelefono = document.getElementById("telefonoPaciente");
+const campoDomicilio = document.getElementById("domicilioPaciente");
+const campoEmail = document.getElementById("emailPaciente");
+const campoDescripcion = document.getElementById("descripcionPaciente");
+
+const buttonCargar = document.getElementById("buttonCargar");
+
+let nombre = "";
+let apellido = "";
+let dni = "";
+let fn = "";
+let sexo = "";
+let os = "";
+let telefono = "";
+let domicilio = "";
+let email = "";
+let descripcion = "";
+
+//const paciente1= new Paciente(nombre, apellido, dni, fn, sexo, os, telefono, domicilio, email, descripcion);
+//console.log(paciente1);
+campoNombre.addEventListener("blur", (e) => {
+  if (validateName(e.target.value, campoNombre)) {
+    //  console.log(e);
+    nombre = e.target.value;
+  }
+});
+campoApellido.addEventListener("blur", (e) => {
+  if (validateName(e.target.value, campoApellido)) {
+    apellido = e.target.value;
+  }
+});
+
+campoDNI.addEventListener("blur", (e) => {
+  if (validateNumber(e.target.value, campoDNI)) {
+    dni = e.target.value;
+  }
+});
+
+campoFecha.addEventListener("blur", (e) => {
+  fn = e.target.value;
+});
+
+campoSexo.addEventListener("blur", (e) => {
+  sexo =campoSexo.value;
+});
+
+campoObraSocial.addEventListener("blur", (e) => {
+  os = campoObraSocial.value;
+});
+
+campoTelefono.addEventListener("blur", (e) => {
+  if (validateNumber(e.target.value, campoTelefono)) {
+    telefono = e.target.value;
+  }
+});
+
+campoDomicilio.addEventListener("blur", (e) => {
+  if (validateName(e.target.value, campoDomicilio)) {
+    //  console.log(e);
+    domicilio = e.target.value;
+  }
+});
+
+campoEmail.addEventListener("blur", (e) => {
+  if (validateEmail(e.target.value, campoEmail)) {
+    email = e.target.value;
+  }
+});
+
+campoDescripcion.addEventListener("blur", (e) => {
+  descripcion = e.target.value;
+});
+
+formularioPaciente.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const paciente1 = new Paciente(
+    nombre,
+    apellido,
+    dni,
+    fn,
+    sexo,
+    os,
+    telefono,
+    domicilio,
+    email,
+    descripcion
+  );
+
+  console.log(paciente1);
+});
