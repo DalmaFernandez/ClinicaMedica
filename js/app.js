2c6a25d94b1a6f7adf512a51beb019a07d2a64a0
Dalma Fernandez <dalmagiufernandez@gmail.com>
2022-10-17 16:02:40 -0300

    modificaciones en footer y clave de acceso

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ const campoClave= document.getElementById("claveDeAcceso");
 const formularioAcceso = document.getElementById("formularioAcceso");
 const botonAdmin = document.getElementById("botonAdmin");
 const modalAcceso = document.getElementById("accederAdmin");
+const mainAdmin = document.getElementsByClassName("mainAdmin");
+let acceso = false;
 let claveIngresada= "";
 
 campoClave.addEventListener("blur",(e)=>{
@@ -14,9 +16,12 @@ claveIngresada = e.target.value;
 formularioAcceso.addEventListener("submit",(e)=>{
     e.preventDefault();
     if ( claveIngresada === clave){
-      
+    acceso = true;
+    sessionStorage.setItem("acceso", acceso);
     window.open("./administrador.html");
-    window.location.reload();  
+    sessionStorage.removeItem("acceso");
+   window.location.reload();  
+    
     //    botonAdmin.style.visibility = "hidden"
     //     administrador.style.visibility= "visible"; //"visible"
     }
