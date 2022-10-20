881deced07f953cb84a5c75542e98b86649ceba1
Juan Pablo Alba Fernandez <juanpialba@gmail.com>
2022-10-19 21:30:57 -0300

    Se crea la pagina de contacto para revision

---
+++ js/app.js
@@ -0,0 +1,30 @@
+const administrador = document.getElementById("administrador");
+
+const clave = "1234";
+const campoClave = document.getElementById("claveDeAcceso");
+const formularioAcceso = document.getElementById("formularioAcceso");
+const botonAdmin = document.getElementById("botonAdmin");
+const modalAcceso = document.getElementById("accederAdmin");
+const mainAdmin = document.getElementsByClassName("mainAdmin");
+let acceso = false;
+let claveIngresada = "";
+
+campoClave.addEventListener("blur", (e) => {
+  claveIngresada = e.target.value;
+});
+formularioAcceso.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (claveIngresada === clave) {
+    acceso = true;
+    sessionStorage.setItem("acceso", acceso);
+    window.open("./administrador.html");
+    sessionStorage.removeItem("acceso");
+    // window.location.reload();
+    window.close();
+
+    //    botonAdmin.style.visibility = "hidden"
+    //     administrador.style.visibility= "visible"; //"visible"
+  } else {
+    campoClave.classList.add("is-invalid");
+  }
+});
